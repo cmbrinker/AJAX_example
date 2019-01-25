161673c15e018c7a7a767c65a6bf91661e146f4a
Chris Brinker <cmbrinker@gmail.com>
2019-01-25 17:13:31 -0500

    button response

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var request = XMLHttpRequest();
+var button = document.getElementById('btn');
 
 request.open('GET', 'https://learnwebcode.github.io/json-e...');
 request.onload = function() {
@@ -6,3 +7,12 @@ request.onload = function() {
   console.log(bookData[0]);
 };
 request.send();
+
+button.addEventListener('click', function() {
+  request.open('GET', 'https://learnwebcode.github.io/json-e...');
+  request.onload = function() {
+    var bookData = JSON.parse(ourRequest.responseText);
+    renderHTML(bookData);
+  };
+  request.send();
+});
